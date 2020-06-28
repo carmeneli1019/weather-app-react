45236d7be6601ec6d7420dc49a934dac8a4fb40c
carmeneli1019 <carmeneli1019@users.noreply.github.com>
2020-06-28 06:30:36 +0100

    Added link to github repository

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="Footer">
-      Coded by Carmen Rosales{" "}
+      <a
+        href="https://github.com/carmeneli1019/weather-app-react"
+        target="_blank"
+      >
+        Open-source code
+      </a>{" "}
+      by Carmen Rosales{" "}
       <span role="img" aria-label="woman coding">
         👩‍💻
       </span>
