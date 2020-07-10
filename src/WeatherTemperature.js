a5b2df64816cc6ff4beeb7767aefc550fced1721
carmeneli1019 <carmeneli1019@users.noreply.github.com>
2020-07-10 15:35:47 +0100

    Implemented unit conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <span className="WeatherTemperature">
-      <h1>{props.temperature}</h1>
-      <span className="units">
-        <a href="/">ºC </a>|<a href="/"> ºF</a>
+  const [units, setUnits] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnits("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnits("celsius");
+  }
+
+  function fahrenheit() {
+    return Math.round((props.temperature * 9) / 5 + 32);
+  }
+
+  if (units === "celsius") {
+    return (
+      <span className="WeatherTemperature">
+        <h1>{props.temperature}</h1>
+        <span className="units">
+          ºC |
+          <a href="/" onClick={showFahrenheit}>
+            {" "}
+            ºF
+          </a>
+        </span>
       </span>
-    </span>
-  );
+    );
+  } else {
+    return (
+      <span className="WeatherTemperature">
+        <h1>{fahrenheit(props.temperature)}</h1>
+        <span className="units">
+          <a href="/" onClick={showCelsius}>
+            ºC{" "}
+          </a>
+          | ºF
+        </span>
+      </span>
+    );
+  }
 }
