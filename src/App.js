d6004be489a356dbe6f49fdeadea4753265ee693
carmeneli1019 <carmeneli1019@users.noreply.github.com>
2020-07-12 21:07:37 +0100

    Moved footer to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import WeatherApp from "./WeatherApp";
-import Footer from "./Footer";
 import "./App.css";
 
 function App() {
@@ -9,7 +8,18 @@ function App() {
       <div className="App">
         <WeatherApp defaultCity="New York" />
       </div>
-      <Footer />
+      <footer className="footer">
+        <a href="https://github.com/carmeneli1019/weather-app-react" target="#">
+          Open-source code
+        </a>{" "}
+        by Carmen Rosales,{" "}
+        <a href="https://frosty-kirch-dc02ac.netlify.app/" target="#">
+          hosted on Netlify{" "}
+        </a>
+        <span role="img" aria-label="woman coding">
+          👩‍💻
+        </span>
+      </footer>
     </div>
   );
 }
